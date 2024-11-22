90f486c04639c0fd3b946bc754371baf7243d968
nbware <nbware@DESKTOP-PBJ7OJ7>
2024-11-22 15:53:34 +0900

    refactor: DateUtil 날짜 format 유형 상수 정의

--- src/utils/DateUtil.java
+++ src/utils/DateUtil.java
@@ -1,5 +1,20 @@
 package utils;
 
 public class DateUtil {
+	
+	// 날짜 포맷 (Date formats)
+	public final static String DATE_FORMAT_COMPACT = "yyyyMMdd";
+	public final static String DATE_FORMAT_STANDARD = "yyyy-MM-dd";
+	public final static String DATE_FORMAT_MONTH_COMPACT = "yyyyMM";
+	public final static String DATE_FORMAT_MONTH_STANDARD = "yyyy-MM";
 
+	// 시간 포맷 (Time formats)
+	public final static String TIME_FORMAT_COMPACT = "HHmmss";
+	public final static String TIME_FORMAT_STANDARD = "HH:mm:ss";
+	
+	// 날짜시간 포맷 (Datetime formats)
+	public final static String DATETIME_FORMAT_COMPACT = "yyyyMMddHHmmss";
+	public final static String DATETIME_FORMAT_STANDARD = "yyyy-MM-dd HH:mm:ss";
+	public final static String DATETIME_FORMAT_MILLIS_COMPACT = "yyyyMMddHHmmssSSS";
+	public final static String DATETIME_FORMAT_MILLIS_STANDARD = "yyyy-MM-dd HH:mm:ss.SSS";
 }
